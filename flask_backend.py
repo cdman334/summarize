f019089e8fc27f47c1de0e1309d012a91e1ff900
cdman334 <cdman334@gmail.com>
2023-10-10 18:24:27 -0400

    Updated flask backend

--- flask_backend.py
+++ flask_backend.py
@@ -3,9 +3,8 @@ import PyPDF2
 from docx import Document
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.probability import FreqDist
-from nltk.tokenize import word_tokenize
 from nltk.cluster.util import cosine_distance
 
 # Download NLTK resources
@@ -42,44 +41,28 @@ def generate_summary(text, num_sentences=3):
             else:
                 sentence_scores[sentences[i]] += score
 
-    # Sort sentences by their scores
     sorted_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)
-
-    # Select the top 'num_sentences' sentences as the summary
     summary = sorted_sentences[:num_sentences]
-
     return " ".join(summary)
 
-# Read the contents of the file (DOCX or PDF)
-def read_file(file_path):
-    if file_path.endswith('.docx'):
-        doc = Document(file_path)
-        text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
-    elif file_path.endswith('.pdf'):
-        with open(file_path, 'rb') as pdf_input:
-            pdf_reader = PyPDF2.PdfFileReader(pdf_input)
-            text = ""
-            for page_num in range(pdf_reader.numPages):
-                page = pdf_reader.getPage(page_num)
-                text += page.extractText()
-    else:
-        raise ValueError("Unsupported file format")
+app = Flask(__name__)
 
-    return text
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify(error="No file part"), 400
 
-# Replace 'your_input_file.docx' or 'your_input_file.pdf' with your file path
-input_file = 'Ethics Situation Analysis.docx'  # Change to your input file
-output_summary_file = 'summary.txt'
+    uploaded_file = request.files['file']
+    if uploaded_file.filename == '':
+        return jsonify(error="No selected file"), 400
 
-# Read the contents of the file
-file_contents = read_file(input_file)
+    file_content = uploaded_file.read().decode('utf-8')
+    try:
+        summary = generate_summary(file_content)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
-# Generate a summary
-summary = generate_summary(file_contents)
+    return jsonify(summary=summary)
 
-# Write the summary to an output file
-with open(output_summary_file, 'w', encoding='utf-8') as summary_output:
-    summary_output.write(summary)
-
-# Print the summary to the console
-print(summary)
+if __name__ == "__main__":
+    app.run(port=5000)
